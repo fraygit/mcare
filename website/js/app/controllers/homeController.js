ce2d11fccb2e651ef94a4486d08a05b819017b54
fraygit <francis.yanga@gmail.com>
2016-11-04 17:07:14 +1300

    chart mockups

--- website/js/app/controllers/homeController.js
+++ website/js/app/controllers/homeController.js
@@ -6,4 +6,63 @@
         defaultView: 'agendaDay'
     })
 
+    var data = {
+        labels: ["January", "February", "March", "April", "May", "June", "July"],
+        datasets: [
+            {
+                label: "Claims submitted to Moh",
+                backgroundColor: 'rgba(54, 162, 235, 0.2)',
+                borderColor: 'rgba(54, 162, 235, 1)',
+                borderWidth: 1,
+                data: [65, 59, 80, 81, 56, 55, 40],
+            }
+        ]
+    };
+
+    var claimsChartEL = document.getElementById("claimsChart");
+    var claimsChart = new Chart(claimsChartEL, {
+        type: "bar",
+        data: data,
+        options: {
+            scales: {
+                xAxes: [{
+                    stacked: true
+                }],
+                yAxes: [{
+                    stacked: true
+                }]
+            }
+        }
+    });
+
+
+    var expenseData = {
+        labels: ["January", "February", "March", "April", "May", "June", "July"],
+        datasets: [
+            {
+                label: "Claims submitted to Moh",
+                backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                borderColor: 'rgba(255,99,132,1)',
+                borderWidth: 1,
+                data: [65, 59, 80, 81, 56, 55, 40],
+            }
+        ]
+    };
+
+    var expenseChartEl = document.getElementById("expenseChart");
+    var expenseChart = new Chart(expenseChartEl, {
+        type: "bar",
+        data: expenseData,
+        options: {
+            scales: {
+                xAxes: [{
+                    stacked: true
+                }],
+                yAxes: [{
+                    stacked: true
+                }]
+            }
+        }
+    });
+
 }]);
--- website/js/app/controllers/patientprofileController.js
+++ website/js/app/controllers/patientprofileController.js
@@ -6,6 +6,8 @@
         labels: [new Date(2016, 1, 1), new Date(2016, 2, 1), new Date(2016, 3, 5), new Date(2016, 4, 1), new Date(2016, 5, 1), new Date(2016, 5, 15), new Date(2016, 5, 31), new Date(2016, 6, 1), new Date(2016, 6, 14)],
         datasets: [
             {
+                backgroundColor: 'rgba(54, 162, 235, 0.2)',
+                borderColor: 'rgba(54, 162, 235, 1)',
                 label: "Weight",
                 data: [65, 59, 80, 81, 56, 55, 40, 60, 70]
             }]
@@ -41,11 +43,15 @@
         labels: [new Date(2016, 1, 1), new Date(2016, 2, 1), new Date(2016, 3, 5), new Date(2016, 4, 1), new Date(2016, 5, 1), new Date(2016, 5, 15), new Date(2016, 5, 31), new Date(2016, 6, 1), new Date(2016, 6, 14)],
         datasets: [
             {
+                backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                borderColor: 'rgba(75, 192, 192, 1)',
                 label: "Systolic",
                 data: [120, 120, 80, 100, 90, 120, 85, 100, 110],
                 strokeColor: "rgba(200,200,200,1)"
             },
             {
+                backgroundColor: 'rgba(255, 206, 86, 0.2)',
+                borderColor: 'rgba(255, 206, 86, 1)',
                 label: "Diastolic",
                 data: [80, 75, 80, 60, 65, 80, 85, 70, 90],
                 strokeColor: "rgba(200,200,200,1)"
